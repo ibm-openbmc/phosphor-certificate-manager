9a499c93d09131fc1d0d12946548307a1dd24524
Ravi Teja <raviteja28031990@gmail.com>
2024-01-19 09:36:29 -0600

    fix lg2 compilation errors

--- certs_manager.cpp
+++ certs_manager.cpp
@@ -28,6 +28,7 @@
 #include <fstream>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/lg2.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/exception.hpp>
